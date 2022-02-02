9cfbecea51a2fe0cc5546f3e195a3f12ef299fc5
JSPromisel <jpromisel@sayarianalytics.com>
2022-02-02 15:40:28 -0500

    update version to 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as infile:
 
 setuptools.setup(
     name="pyisic",
-    version="0.1.2",
+    version="0.1.3",
     author="Jordan Vani",
     author_email="tech@sayari.com",
     description="Standard industrial classification standardization",
