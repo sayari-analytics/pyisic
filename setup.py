f0d34fe3fa1c5870a45429435a6c47d15f682e38
JSPromisel <46266113+JSPromisel@users.noreply.github.com>
2024-05-21 17:07:38 -0400

    update version to 0.1.19

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as infile:
 
 setuptools.setup(
     name="pyisic",
-    version="0.1.18",
+    version="0.1.19",
     author="Jordan Vani",
     author_email="tech@sayari.com",
     description="Standard industrial classification standardization",
